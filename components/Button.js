451c2dbff459e5878b216d5ef5bebe60a6308eee
Kevin Huang <kevin.huang@genmills.com>
2022-06-21 16:34:24 -0500

    edit gitignore

--- components/Button.js
+++ components/Button.js
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types';
+
 export default function Button({ label }) {
     return (
         <button>{label}</button>
     );
+}
+
+Button.PropTypes = {
+    label: PropTypes.string
 }
