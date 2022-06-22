5b6c8d534ace3d4290a4e13c5b32f07c52fe69fe
Kevin Huang <kevin.huang@genmills.com>
2022-06-22 12:48:46 -0500

    remove proptypes import

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,5 @@
-import PropTypes from 'prop-types';
-
 export default function Button({ label }) {
     return (
         <button>{label}</button>
     );
-}
-
-Button.PropTypes = {
-    label: PropTypes.string
 }
